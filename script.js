c33d4c76d0aa241b393c4e66bbdb76d94d12cade
Bastien VILLEGAS <95182085+Bastien64@users.noreply.github.com>
2023-08-22 23:25:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        $('.animated-from-right').each(function() {
+            var top_of_element = $(this).offset().top;
+            var bottom_of_element = top_of_element + $(this).outerHeight();
+            var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+
+            if (bottom_of_screen > top_of_element) {
+                $(this).addClass('animate');
+            }
+        });
+    });
+});
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        var scrollTop = $(window).scrollTop();
+        var videoOffset = $('#background-video').offset().top;
+        var videoHeight = $('#background-video').height();
+
+        // Calculez le pourcentage de visibilité de la vidéo
+        var visibilityPercentage = 1 - (videoOffset - scrollTop) / videoHeight;
+        visibilityPercentage = Math.min(1, Math.max(0, visibilityPercentage)); // Assurez-vous qu'il reste entre 0 et 1
+
+        // Convertissez le pourcentage de visibilité en opacité et en filtre de luminosité
+        var opacity = visibilityPercentage;
+        var brightness = 1- visibilityPercentage;
+
+        $('#background-video').css({
+            'opacity': opacity,
+            'filter': 'brightness(' + brightness + ')'
+        });
+    });
+});
+$(document).ready(function() {
+    $(window).scroll(function() {
+        var scrollTop = $(window).scrollTop();
+        var videoOffset = $('#background-video2').offset().top;
+        var videoHeight = $('#background-video2').height();
+
+        // Calculez le pourcentage de visibilité de la vidéo
+        var visibilityPercentage = 1 - (videoOffset - scrollTop) / videoHeight;
+        visibilityPercentage = Math.min(1, Math.max(0, visibilityPercentage)); // Assurez-vous qu'il reste entre 0 et 1
+
+        // Convertissez le pourcentage de visibilité en opacité et en filtre de luminosité
+        var opacity = visibilityPercentage;
+        var brightness = 1- visibilityPercentage;
+
+        $('#background-video2').css({
+            'opacity': opacity,
+            'filter': 'brightness(' + brightness + ')'
+        });
+    });
+});
