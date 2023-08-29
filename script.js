121f156928ca717b1a7a76bc15b9e3c3a263fea6
Bastien VILLEGAS <95182085+Bastien64@users.noreply.github.com>
2023-08-29 22:22:51 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -12,26 +12,7 @@ $(document).ready(function() {
     });
 });
 
-$(document).ready(function() {
-    $(window).scroll(function() {
-        var scrollTop = $(window).scrollTop();
-        var videoOffset = $('#background-video').offset().top;
-        var videoHeight = $('#background-video').height();
 
-        // Calculez le pourcentage de visibilité de la vidéo
-        var visibilityPercentage = 1 - (videoOffset - scrollTop) / videoHeight;
-        visibilityPercentage = Math.min(1, Math.max(0, visibilityPercentage)); // Assurez-vous qu'il reste entre 0 et 1
-
-        // Convertissez le pourcentage de visibilité en opacité et en filtre de luminosité
-        var opacity = visibilityPercentage;
-        var brightness = 1- visibilityPercentage;
-
-        $('#background-video').css({
-            'opacity': opacity,
-            'filter': 'brightness(' + brightness + ')'
-        });
-    });
-});
 const questions = document.querySelectorAll('.question');
 questions.forEach(question => {
     question.addEventListener('click', () => {
